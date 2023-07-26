da2ac1b93dce89d46f0c129f1fffa081e0e0ee2e
consansri <constantin.birkert@web.de>
2023-07-26 12:28:27 +0200

    added wasm target

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,24 +23,36 @@ kotlin {
     }
 
     wasm {
-        this.applyBinaryen()
-        browser()
+        binaries.executable()
+        browser {
+
+        }
     }
 
     sourceSets {
-        val jsMain by getting {
+        val commonMain by getting {
+            dependencies {
+
+            }
+        }
+        val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
+            }
+        }
+        val jsMain by getting {
+            dependencies {
                 implementation("org.jetbrains.kotlin-wrappers:kotlin-react:18.2.0-pre.346")
                 implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom:18.2.0-pre.346")
                 implementation("org.jetbrains.kotlin-wrappers:kotlin-emotion:11.9.3-pre.346")
-
             }
         }
 
         val wasmMain by getting {
             dependencies {
-                implementation(kotlin("test"))
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-core-wasm:1.5.1-wasm0")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-wasm:1.6.4-wasm0")
+                implementation("io.ktor:ktor-client-core-wasm:2.3.1-wasm0")
             }
         }
     }
