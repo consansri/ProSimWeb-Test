64c1b82fc05b05976255d70f5aa7d0f76e4c15f7
consansri <constantin.birkert@web.de>
2023-10-23 16:18:59 +0200

    Version 0.1.0

--- src/jsMain/kotlin/emulator/Emulator.kt
+++ src/jsMain/kotlin/emulator/Emulator.kt
@@ -1,9 +1,6 @@
 package emulator
 
 import emulator.kit.Architecture
-import emulator.cisc.ArchRV64
-import emulator.cisc.ArchRV32
-import emulator.kit.TestObj
 
 /**
  * This Class Contains the whole Architecture States and is the main connection to the visual view components.
@@ -19,10 +16,6 @@ class Emulator {
      * [archList] contains one object of each specific Architecture.
      */
     private val archList: List<Link> = Link.entries.toList()
-
-    init {
-        TestObj.calc()
-    }
 
     fun getArchList(): List<Architecture> {
         return archList.map { it.architecture }
