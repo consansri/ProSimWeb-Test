11e1376def3676ee3e3c18b72d71cde37251d50a
consansri <constantin.birkert@web.de>
2023-11-04 15:49:43 +0100

    version 0.1.2

--- src/jsMain/kotlin/Constants.kt
+++ src/jsMain/kotlin/Constants.kt
@@ -1,7 +1,7 @@
 object Constants {
 
     const val name = "ProSimWeb"
-    const val version = "0.1.1"
+    const val version = "0.1.2"
     const val year = "2023"
     const val org = "Universität Stuttgart IKR"
 
