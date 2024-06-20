035fdf3297496605c6fea17c915fc5f151ee7d73
consansri <constantin.birkert@web.de>
2024-06-20 12:57:18 +0200

    JVM: CSpinner now commits textfield edited values.

--- src/jvmMain/kotlin/me/c3/ui/styled/CSpinner.kt
+++ src/jvmMain/kotlin/me/c3/ui/styled/CSpinner.kt
@@ -1,9 +1,12 @@
 package me.c3.ui.styled
 
+import emulator.kit.nativeLog
 import me.c3.ui.styled.params.BorderMode
 import me.c3.ui.styled.params.FontType
 import javax.swing.JSpinner
 import javax.swing.SpinnerModel
+import javax.swing.event.DocumentEvent
+import javax.swing.event.DocumentListener
 
 class CSpinner(model: SpinnerModel, fontType: FontType = FontType.BASIC,val borderMode: BorderMode = BorderMode.HORIZONTAL) : JSpinner(model) {
 
@@ -15,6 +18,32 @@ class CSpinner(model: SpinnerModel, fontType: FontType = FontType.BASIC,val bord
 
     init {
         setUI(CSpinnerUI())
+        val editor = editor
+
+        if(editor is DefaultEditor){
+            editor.textField.document.addDocumentListener(object : DocumentListener{
+                override fun insertUpdate(e: DocumentEvent?) {
+                    commitEditorValue()
+                }
+
+                override fun removeUpdate(e: DocumentEvent?) {
+                    commitEditorValue()
+                }
+
+                override fun changedUpdate(e: DocumentEvent?) {
+                    commitEditorValue()
+                }
+            })
+        }
+    }
+
+    fun commitEditorValue(){
+        try {
+            nativeLog("Committing Edit!")
+            commitEdit()
+        }catch (e: Exception){
+            // parse exception handling not needed
+        }
     }
 
 }
