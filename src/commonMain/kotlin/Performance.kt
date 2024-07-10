50d850e78e2d7248abb0d43cd3171df4b5462ac4
consansri <constantin.birkert@web.de>
2024-07-10 16:46:26 +0200

    KIT: Fixed the possible division by zero in Performance.updateExePerformance, which led to not executing instructions.

--- src/commonMain/kotlin/Performance.kt
+++ src/commonMain/kotlin/Performance.kt
@@ -5,7 +5,9 @@ object Performance {
     var MAX_INSTR_EXE_AMOUNT = 1000L
 
     fun updateExePerformance(instrCount: Long, ellapsed: Duration) {
-        MAX_INSTR_EXE_AMOUNT = 1000000000 / (ellapsed.inWholeNanoseconds / instrCount)
+        if (instrCount > 0L && ellapsed.inWholeNanoseconds > 0L) {
+            MAX_INSTR_EXE_AMOUNT = 1000000000 / (ellapsed.inWholeNanoseconds / instrCount)
+        }
     }
 
 }
