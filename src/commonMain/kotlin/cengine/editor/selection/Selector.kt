06f6973206d066514a9c72f4a0b84598c796a4f5
consansri <constantin.birkert@web.de>
2024-07-11 15:26:00 +0200

    KIT: Improved Selector.moveCaretUp,Down,Left,Right for a already selected region.

--- src/commonMain/kotlin/cengine/editor/selection/Selector.kt
+++ src/commonMain/kotlin/cengine/editor/selection/Selector.kt
@@ -26,23 +26,51 @@ interface Selector {
     }
 
     fun moveCaretLeft(offset: Int, shift: Boolean) {
-        val newIndex = (caret.index - offset).coerceAtLeast(0)
+        val range = selection.asRange()
+        val newIndex = if (!shift && range != null && range.first != caret.index) {
+            // move the caret to the beginning of the selection if it isn't already there.
+            range.first
+        } else {
+            (caret.index - offset).coerceAtLeast(0)
+        }
+
         moveCaretTo(newIndex, shift)
     }
 
     fun moveCaretRight(offset: Int, shift: Boolean) {
-        val newIndex = (caret.index + offset).coerceAtMost(caret.model.length)
+        val range = selection.asRange()
+        val newIndex = if (!shift && range != null && range.last + 1 != caret.index) {
+            // move the caret to the end of the selection if it isn't already there.
+            range.last + 1
+        } else {
+            (caret.index + offset).coerceAtMost(caret.model.length)
+        }
+
         moveCaretTo(newIndex, shift)
     }
 
     fun moveCaretUp(offset: Int, shift: Boolean) {
-        val newLine = (caret.line - offset).coerceAtLeast(0)
+        val range = selection.asRange()
+
+        val newLine = if (!shift && range != null && range.first != caret.index) {
+            // move the caret above the current selection if it isn't already at the lower bound of the selection.
+            (caret.model.getLineAndColumn(range.first).first - 1).coerceAtLeast(0)
+        } else {
+            (caret.line - offset).coerceAtLeast(0)
+        }
         val newIndex = caret.model.getIndexFromLineAndColumn(newLine, caret.col)
         moveCaretTo(newIndex, shift)
     }
 
     fun moveCaretDown(offset: Int, shift: Boolean) {
-        val newLine = (caret.line + offset).coerceAtMost(caret.model.lines)
+        val range = selection.asRange()
+
+        val newLine = if (!shift && range != null && range.last + 1 != caret.index) {
+            // move the caret under the current selection if it isn't already at the higher bound of the selection.
+            (caret.model.getLineAndColumn(range.last + 1).first + 1).coerceAtMost(caret.model.lines)
+        } else {
+            (caret.line + offset).coerceAtMost(caret.model.lines)
+        }
         val newIndex = caret.model.getIndexFromLineAndColumn(newLine, caret.col)
         moveCaretTo(newIndex, shift)
     }
