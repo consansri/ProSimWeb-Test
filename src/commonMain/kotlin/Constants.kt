7d7fa09dbc1a58bd5035fe92f51369fe551f5d53
consansri <constantin.birkert@web.de>
2024-12-03 09:32:00 +0100

    Changed Version Number to 1.0.0

--- src/commonMain/kotlin/Constants.kt
+++ src/commonMain/kotlin/Constants.kt
@@ -2,11 +2,6 @@ import config.BuildConfig
 
 data object Constants {
     const val NAME = BuildConfig.NAME
-    val WEBNAME = "${NAME}Web"
-    val JVMNAME= "${NAME}JVM"
     const val VERSION = BuildConfig.VERSION
-    const val YEAR = BuildConfig.YEAR
-    const val ORG = BuildConfig.ORG
-    const val DEV = BuildConfig.DEV
     const val TITLE = "$NAME - $VERSION"
 }
