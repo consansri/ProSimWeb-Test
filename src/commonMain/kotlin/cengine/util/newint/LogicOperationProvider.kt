23a46c60f0b491f7b55d45e04e329f40350ea2f2
consansri <constantin.birkert@web.de>
2024-12-10 20:32:07 +0100

    ENGINE: Implemented IntNumber.signExtension and IntNumber.compareTo Int, Long

--- src/commonMain/kotlin/cengine/util/newint/LogicOperationProvider.kt
+++ src/commonMain/kotlin/cengine/util/newint/LogicOperationProvider.kt
@@ -2,19 +2,22 @@ package cengine.util.newint
 
 interface LogicOperationProvider<in T: Any, out U: Any> {
 
+    val bitWidth: Int
+
     operator fun inv(): U
     infix fun and(other: T): U
     infix fun or(other: T): U
     infix fun xor(other: T): U
-    infix fun shl(other: T): U
-    infix fun shr(other: T): U
+    infix fun shl(bits: T): U
+    infix fun shr(bits: T): U
 
     // Kotlin Int Operations
     infix fun and(other: Int): U
     infix fun or(other: Int): U
     infix fun xor(other: Int): U
-    infix fun shl(other: Int): U
-    infix fun shr(other: Int): U
+    infix fun shl(bits: Int): U
+    infix fun shr(bits: Int): U
+
 
     // Kotlin Int Operations
     infix fun and(other: Long): U
