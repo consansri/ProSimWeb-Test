7d226e2ca5481755a9f57f0cd5e6614b8a3087e4
consansri <constantin.birkert@web.de>
2024-12-12 16:48:20 +0100

    ENGINE: Introducing IntNumberStatic which helps on reducing Unknown Type Variables.

--- src/commonMain/kotlin/cengine/util/newint/Int16.kt
+++ src/commonMain/kotlin/cengine/util/newint/Int16.kt
@@ -11,15 +11,21 @@ class Int16(override val value: Short) : IntNumber<Int16> {
     constructor(value: Int) : this(value.toShort())
     constructor(value: Long) : this(value.toShort())
 
-    companion object {
-        val ZERO = Int16(0)
-        val ONE = Int16(1)
+    companion object: IntNumberStatic<Int16> {
+        override val ZERO = Int16(0)
+
+
+        override val ONE = Int16(1)
 
         fun Short.toInt16() = Int16(this)
-        fun String.parseInt16(radix: Int): Int16 = Int16(toShort(radix))
         fun fromUInt8(byte1: UInt8, byte0: UInt8): Int16 = (byte1.toInt16() shl 8) or byte0.toInt16()
 
-        fun createBitMask(bitWidth: Int): Int16 {
+        override fun to(number: IntNumber<*>): Int16 = number.toInt16()
+        override fun split(number: IntNumber<*>): List<Int16> = number.int16s()
+        override fun of(value: Int): Int16 = Int16(value.toShort())
+        override fun parse(string: String, radix: Int): Int16 = Int16(string.toShort(radix))
+
+        override fun createBitMask(bitWidth: Int): Int16 {
             require(bitWidth in 0..16) { "$bitWidth exceeds 0..16"}
             return (ONE shl bitWidth) - 1
         }
@@ -81,7 +87,10 @@ class Int16(override val value: Short) : IntNumber<Int16> {
     override fun compareTo(other: Int16): Int = value.compareTo(other.value)
     override fun compareTo(other: Long): Int = value.compareTo(other)
     override fun compareTo(other: Int): Int = value.compareTo(other)
-    override fun equals(other: Any?): Boolean = if (other is Int16) value == other.value else false
+    override fun equals(other: Any?): Boolean {
+        if (other is IntNumber<*>) return value == other.value
+        return value == other
+    }
 
     override fun toInt8(): Int8 = Int8(value.toByte())
 
@@ -95,22 +104,21 @@ class Int16(override val value: Short) : IntNumber<Int16> {
     override fun toUInt16(): UInt16 = UInt16(value.toUShort())
     override fun toUInt32(): UInt32 = UInt32(value.toUInt())
     override fun toUInt64(): UInt64 = UInt64(value.toULong())
+    override fun toUInt128(): UInt128 = UInt128(BigInteger.fromUShort(value.toUShort()))
+
+    override fun toUnsigned(): UInt16 = toUInt16()
 
     override fun toString(radix: Int): String = value.toString(radix)
     override fun toString(): String = value.toString()
 
     override fun fitsInSigned(bitWidth: Int): Boolean {
-        if (bitWidth >= bitWidth) return true
+        if (bitWidth >= this.bitWidth) return true
         val minValue = -(ONE shl (bitWidth - 1)) // -2^(bitWidth-1)
         val maxValue = (ONE shl (bitWidth - 1)) - 1 // 2^(bitWidth-1) - 1
         return value in minValue.value..maxValue.value
     }
 
-    override fun fitsInUnsigned(bitWidth: Int): Boolean {
-        if (bitWidth >= bitWidth) return true
-        val maxValue = (ONE shl bitWidth) - 1 // 2^bitWidth - 1
-        return value in ZERO.value..maxValue.value
-    }
+    override fun fitsInUnsigned(bitWidth: Int): Boolean = toUInt16().fitsInUnsigned(bitWidth)
 
     override fun hashCode(): Int {
         return value.hashCode()
